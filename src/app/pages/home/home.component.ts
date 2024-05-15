c3ee0858740612fba30cbedd4f5f8aaaf2610bcb
egglou <miguel.rafaellouie2003@gmail.com>
2024-05-15 23:29:37 +0800

    partial home

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { HeroComponent } from '../../components/layout/hero/hero.component';
 import { FooterComponent } from '../../components/layout/footer/footer.component';
+import { WhatWeOfferComponent } from '../../components/layout/what-we-offer/what-we-offer.component';
 
 @Component({
     selector: 'app-home',
@@ -8,6 +9,7 @@ import { FooterComponent } from '../../components/layout/footer/footer.component
   imports: [
     HeroComponent,
     FooterComponent,
+    WhatWeOfferComponent,
   ],
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
