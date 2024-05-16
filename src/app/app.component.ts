76b7d4a6054287078b17315a6ba6eaba4d4c0619
egglou <miguel.rafaellouie2003@gmail.com>
2024-05-16 12:22:10 +0800

    done home

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { AxiosService } from './services/axios.service';
 import { Title } from '@angular/platform-browser';
@@ -10,6 +10,7 @@ import { MasterComponent } from './layout/master/master.component';
     imports: [RouterOutlet, MasterComponent],
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class AppComponent {
     title = 'Stream Health';
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HeroComponent } from '../../components/layout/hero/hero.component';
 import { FooterComponent } from '../../components/layout/footer/footer.component';
 import { WhatWeOfferComponent } from '../../components/layout/what-we-offer/what-we-offer.component';
+import { TeamComponent } from '../../components/layout/team/team.component';
 
 @Component({
     selector: 'app-home',
@@ -10,8 +11,10 @@ import { WhatWeOfferComponent } from '../../components/layout/what-we-offer/what
     HeroComponent,
     FooterComponent,
     WhatWeOfferComponent,
+    TeamComponent,
   ],
     templateUrl: './home.component.html',
     styleUrl: './home.component.css',
 })
-export class HomeComponent {}
+export class HomeComponent {
+}
